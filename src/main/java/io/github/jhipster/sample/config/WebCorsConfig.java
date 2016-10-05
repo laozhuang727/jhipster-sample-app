9a616dd2190c6062c4324890e4a48bab2e76a9dd
ryan.zhuang <ryan.zhuang@vipshop.com>
2016-10-05 11:07:37 +0800

    fix the jhistper app start up error

--- src/main/java/io/github/jhipster/sample/config/WebCorsConfig.java
+++ src/main/java/io/github/jhipster/sample/config/WebCorsConfig.java
@@ -1,18 +1,17 @@
-package io.github.jhipster.sample.web.rest;
+package io.github.jhipster.sample.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class WebCorsConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/*")
-            .allowedOrigins("http://127.0.0.1:8085");
+            .allowedOrigins("http://127.0.0.1:8085","http://127.0.0.1:9000");
 //            .allowedMethods("PUT", "DELETE")
 //            .allowedHeaders("header1", "header2", "header3")
 //            .exposedHeaders("header1", "header2")
